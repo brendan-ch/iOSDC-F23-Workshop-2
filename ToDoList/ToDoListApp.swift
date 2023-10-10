26448da0ec9420f2ba51e4da872c1482397642ad
Brendan Chen <smallpigeon1@gmail.com>
2023-10-10 14:50:27 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Brendan on 2023.10.10.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
